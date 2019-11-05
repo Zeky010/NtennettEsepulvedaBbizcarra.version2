4b779b598e777f77e0a0d7eb8d1e762aa9b2d375
tennett <56683255+tennett@users.noreply.github.com>
2019-11-05 18:39:48 -0300

    actualizado 2.0

--- Nueva carpeta (2)/NtennettEsepulvedaBbizcarra/biblio_pelis/bibliocatalogo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BibliocatalogoConfig(AppConfig):
-    name = 'bibliocatalogo'
--- Nueva carpeta (2)/NtennettEsepulvedaBbizcarra/biblio_pelis/bibliocatalogo/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import user
-
-class CrearUsuario(forms.ModelForm):
-    class Meta:
-        model = user
-        fields= [
-            'nombre', 'email', 'password'
-            ]
